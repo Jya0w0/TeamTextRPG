f9626549a3ef3ccfb94ee147ef3cdb8598c62b1b
Jya0w0 <jya0w0@gmail.com>
2023-11-21 12:07:35 +0900

    Create Design.cs

---
+++ TeamTextRPG/Design.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTextRPG
+{
+    internal class Design
+    {
+        public void ConsoleBackground()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        public void LogoFontColor()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Thread.Sleep(50);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Thread.Sleep(50);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Thread.Sleep(50);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(50);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Thread.Sleep(50);
+        }
+    }
+}
